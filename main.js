c34640fd6c02cb9584f619f7350f6f5ae58b84b6
Mahm0udAbbas <mahmoudabbas393@gmail.com>
2024-01-22 01:27:07 +0200

    Upload main project

---
+++ main.js
@@ -0,0 +1,20 @@
+let nums = document.querySelectorAll(".stats .number");
+let section = document.querySelector(".stats");
+started = false;
+window.onscroll = () => {
+  if (window.scrollY >= section.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
